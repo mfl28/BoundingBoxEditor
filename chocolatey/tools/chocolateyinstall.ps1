a827911f498e5a394aa16ac96e5ada2e026d2956
Markus Fleischhacker <markus.fleischhacker28+git@gmail.com>
2020-10-06 08:56:40 +0200

    Add chocolatey package configuration files.

---
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = 'https://github.com/mfl28/BoundingBoxEditor/releases/download/v2.2.0/boundingboxeditor-2.2.0.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  url64bit      = $url64
+  softwareName  = 'boundingboxeditor*'
+  checksum64    = '7D61593EBA0BB90E12931E031670B7793026B0AAA46C701D89F4281C658EFAC1'
+  checksumType64= 'sha256'
+  silentArgs    = '/quiet'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
