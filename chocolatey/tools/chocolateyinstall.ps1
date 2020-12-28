405f5f26793e9be699979e979cd81dcea9daefc6
Markus Fleischhacker <markus.fleischhacker28+git@gmail.com>
2020-12-28 11:59:16 +0100

    Update chocolatey files.

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://github.com/mfl28/BoundingBoxEditor/releases/download/v2.2.0/boundingboxeditor-2.2.0.exe'
+$url64      = 'https://github.com/mfl28/BoundingBoxEditor/releases/download/v2.3.0/boundingboxeditor-2.3.0.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
   url64bit      = $url64
   softwareName  = 'boundingboxeditor*'
-  checksum64    = '7D61593EBA0BB90E12931E031670B7793026B0AAA46C701D89F4281C658EFAC1'
+  checksum64    = '0F78324257400D195A3AB16538B7A0158CE1F4B3266B5C27CEFC6F71B9FD7BBA'
   checksumType64= 'sha256'
   silentArgs    = '/quiet'
   validExitCodes= @(0)
