940591d02d0c34209846cfb4288c0032650691fc
Markus Fleischhacker <markus.fleischhacker28+git@gmail.com>
2023-01-04 13:14:49 +0100

    Update chocolatey release data. (#51)

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://github.com/mfl28/BoundingBoxEditor/releases/download/v2.4.0/boundingboxeditor-2.4.0.exe'
+$url64      = 'https://github.com/mfl28/BoundingBoxEditor/releases/download/v2.5.0/boundingboxeditor-2.5.0.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
   url64bit      = $url64
   softwareName  = 'boundingboxeditor*'
-  checksum64    = '882F57B177B99000C97AC851EC09C75954E4B801399DA37078B9FC36EAF283B6'
+  checksum64    = '909D8704A9CF0502073D6346CC940D2B852BFD36356B42A990DBD5A3E4ADA775'
   checksumType64= 'sha256'
   silentArgs    = '/quiet'
   validExitCodes= @(0)
