84c067d7ee64e092e1f32da82aad4d491e8b444a
Markus Fleischhacker <markus.fleischhacker28+git@gmail.com>
2024-12-27 15:28:55 +0100

    Switch to tensorflow CSV format.

--- src/test/java/com/github/mfl28/boundingboxeditor/model/io/CSVSaveStrategyTest.java
+++ src/test/java/com/github/mfl28/boundingboxeditor/model/io/CSVSaveStrategyTest.java
@@ -20,20 +20,20 @@ package com.github.mfl28.boundingboxeditor.model.io;
 
 import com.github.mfl28.boundingboxeditor.model.data.*;
 import com.github.mfl28.boundingboxeditor.model.io.results.ImageAnnotationExportResult;
-import com.google.common.jimfs.Configuration;
-import com.google.common.jimfs.Jimfs;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.scene.paint.Color;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CSVSaveStrategyTest {
 
@@ -41,81 +41,44 @@ class CSVSaveStrategyTest {
      * One image with two annotations gets exported to CSV.
      */
     @Test
-    void multipleRectangles() throws IOException {
-        ObjectCategory objectCategory = new ObjectCategory("category", Color.YELLOW);
+    void testCSVSaving(@TempDir Path tempDir) throws IOException {
+        var objectCategory1 = new ObjectCategory("catA", Color.YELLOW);
+        var objectCategory2 = new ObjectCategory("catB", Color.BLUE);
+        var imageMetaData1 = new ImageMetaData("sample1.png", "folderName", "url", 100, 200, 0);
+        var imageMetaData2 = new ImageMetaData("sample2.png", "folderName", "url", 400, 300, 0);
 
-        ImageAnnotation imageAnnotation = new ImageAnnotation(new ImageMetaData("sample.png", "folderName", "url", 100, 200, 0));
-        List<BoundingShapeData> boundingShapeDatas = new ArrayList<>();
-        boundingShapeDatas.add(new BoundingBoxData(objectCategory, 0,0,0.5,0.5, List.of("tag")));
-        boundingShapeDatas.add(new BoundingBoxData(objectCategory, 0,0,0.25,0.25, List.of("tag")));
-        imageAnnotation.setBoundingShapeData(boundingShapeDatas);
+        List<BoundingShapeData> boundingShapeData1 = List.of(
+                new BoundingBoxData(objectCategory1, 0, 0, 0.5, 0.5, List.of("tag")),
+                new BoundingBoxData(objectCategory2, 0, 0, 0.25, 0.25, List.of("tag")),
+                new BoundingPolygonData(objectCategory1, List.of(0.0, 0.0, 0.5, 0.5), Collections.emptyList())
+        );
 
-        ImageAnnotationData annotations = new ImageAnnotationData(List.of(imageAnnotation),
-                Map.of("object", 1),
-                Map.of("object", objectCategory));
-        Path destination = Jimfs.newFileSystem(Configuration.unix()).getPath("annotations.csv");
+        var imageAnnotation1 = new ImageAnnotation(imageMetaData1, boundingShapeData1);
+
+        List<BoundingShapeData> boundingShapeData2 = List.of(
+                new BoundingBoxData(objectCategory2, 0.1, 0, 0.5, 0.2, List.of("tag"))
+        );
+
+        var imageAnnotation2 = new ImageAnnotation(imageMetaData2, boundingShapeData2);
+
+        ImageAnnotationData annotations = new ImageAnnotationData(
+                List.of(imageAnnotation1, imageAnnotation2),
+                Map.of("catA", 2, "catB", 2),
+                Map.of("catA", objectCategory1, "catB", objectCategory2));
+
+        Path destination = tempDir.resolve("annotations.csv");
+
         ImageAnnotationExportResult save = new CSVSaveStrategy().save(annotations, destination, new SimpleDoubleProperty(0));
+
         assertTrue(save.getErrorTableEntries().isEmpty());
 
         String content = Files.readString(destination);
+
         assertEquals("""
-                "name","id","label","xMin","xMax","yMin","yMax"
-                "sample.png","0","category","0","50","0","100"
-                "sample.png","1","category","0","25","0","50"
-                """, content);
-    }
-
-    /**
-     * Two images with each one annotation gets exported.
-     */
-    @Test
-    void multipleImages() throws IOException {
-        ObjectCategory objectCategory = new ObjectCategory("category", Color.YELLOW);
-
-        ImageAnnotation imageAnnotation1 = new ImageAnnotation(
-                new ImageMetaData("sample1.png", "folderName", "url", 100, 200, 0),
-                List.of(new BoundingBoxData(objectCategory, 0,0,0.5,0.5, List.of("tag"))));
-
-        ImageAnnotation imageAnnotation2 = new ImageAnnotation(
-                new ImageMetaData("sample2.png", "folderName", "url", 100, 200, 0),
-                List.of(new BoundingBoxData(objectCategory, 0,0,0.25,0.25, List.of("tag"))));
-
-        ImageAnnotationData annotations = new ImageAnnotationData(List.of(imageAnnotation1, imageAnnotation2),
-                Map.of("object", 1),
-                Map.of("object", objectCategory));
-        Path destination = Jimfs.newFileSystem(Configuration.unix()).getPath("annotations.csv");
-        ImageAnnotationExportResult save = new CSVSaveStrategy().save(annotations, destination, new SimpleDoubleProperty(0));
-        assertTrue(save.getErrorTableEntries().isEmpty());
-
-        String content = Files.readString(destination);
-        assertEquals("""
-                "name","id","label","xMin","xMax","yMin","yMax"
-                "sample1.png","0","category","0","50","0","100"
-                "sample2.png","1","category","0","25","0","50"
-                """, content);
-    }
-
-    /**
-     * One image with one annotation should be saved. The annotation uses an unsupported Bounding Shape, so a ErrorTableEntry is expected.
-     */
-    @Test
-    void wrongBoundingShape() throws IOException {
-        ObjectCategory objectCategory = new ObjectCategory("category", Color.YELLOW);
-
-        ImageAnnotation imageAnnotation1 = new ImageAnnotation(
-                new ImageMetaData("sample1.png", "folderName", "url", 100, 200, 0),
-                List.of(new BoundingPolygonData(objectCategory, List.of(0.0, 0.0, 0.5, 0.5), List.of("tag"))));
-
-        ImageAnnotationData annotations = new ImageAnnotationData(List.of(imageAnnotation1),
-                Map.of("object", 1),
-                Map.of("object", objectCategory));
-        Path destination = Jimfs.newFileSystem(Configuration.unix()).getPath("annotations.csv");
-        ImageAnnotationExportResult save = new CSVSaveStrategy().save(annotations, destination, new SimpleDoubleProperty(0));
-        assertEquals(1, save.getErrorTableEntries().size());
-
-        String content = Files.readString(destination);
-        assertEquals("""
-                "name","id","label","xMin","xMax","yMin","yMax"
+                "filename","width","height","class","xmin","ymin","xmax","ymax"
+                "sample1.png","100","200","catA","0","0","50","100"
+                "sample1.png","100","200","catB","0","0","25","50"
+                "sample2.png","400","300","catB","40","0","200","60"
                 """, content);
     }
 }
