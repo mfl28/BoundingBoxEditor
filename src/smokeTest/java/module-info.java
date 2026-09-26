a587c4a478c5d1f0743b6656d4f65e3e6087c1c7
Markus Fleischhacker <markus.fleischhacker28+git@gmail.com>
2026-09-26 16:54:30 +0200

    Enable native access for JavaFX and smoke-test the packaged app in CI.

---
+++ src/smokeTest/java/module-info.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2026 Markus Fleischhacker <markus.fleischhacker28@gmail.com>
+ *
+ * This file is part of Bounding Box Editor
+ *
+ * Bounding Box Editor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bounding Box Editor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Bounding Box Editor. If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * A smoke test run with the Java runtime of the packaged app (the jlink image), see the packagedAppSmokeTest task.
+ */
+module smoketest {
+    requires com.github.mfl28.boundingboxeditor;
+    requires com.github.mfl28.merged.module;
+    requires javafx.base;
+}
